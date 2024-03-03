23ab11c6933a19927ccc7c2a4961010550049953
Gleb <130483785+GP-iOS-23@users.noreply.github.com>
2024-03-03 19:30:46 +0500

    try again.....

--- TinkoffCalculator/HistoryViewController.swift
+++ TinkoffCalculator/HistoryViewController.swift
@@ -11,20 +11,6 @@ class HistoryViewController: UIViewController{
     
     var result: String?
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        initialize()
-    }
-    
-    required init?(coder: NSCoder) {
-        super .init(coder: coder)
-        initialize()
-    }
-    
-    private func initialize(){
-        modalPresentationStyle = .fullScreen
-    }
-    
     @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
